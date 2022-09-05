83d03424e0def9446bf10da5bab70395494d5e1a
Zaur <86913677+Balanovz@users.noreply.github.com>
2022-09-05 16:18:02 +0400

    Add files via upload

---
+++ Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace PREPRI
+{
+    public class Master
+    {
+        public static string Parser(string inputString)
+        {
+            string[] splitWords = inputString.Split(' ');
+            var result = new StringBuilder();
+            string temp = "";
+
+            foreach (var i in splitWords)
+            {
+                if (Regex.IsMatch(i, "[Пп]ри|ие"))
+                {
+                    temp = Regex.Replace(i, @"При", "Пре");
+                    temp = Regex.Replace(temp, @"при", "пре");
+                    temp = Regex.Replace(temp, @"ие", "ее");
+                    result.Append(temp + " ");
+                }
+                else
+                    result.Append(i + " ");
+            }
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ TestPrePri.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PREPRI;
+using System;
+
+namespace UnitTestsPREPRI
+{
+    [TestClass]
+    public class TestPrePri
+    {
+
+        private void TestResult(string expected, string actual)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string testString = "Бытие, \"нашей\" жизни: чрезмерно$ огромное и $присоединение его в другие миры было бы делом непосильным";
+            string expected = "Бытее, \"нашей\" жизни: чрезмерно$ огромное и $пресоединенее его в другее миры было бы делом непосильным";
+            string actual = Master.Parser(testString);
+            TestResult(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string testString = "Привет, новый мир ";
+            string expected = "Превет, новый мир";
+            string actual = Master.Parser(testString);
+            TestResult(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string testString = "$Присоединение: \"$присоединение\".";
+            string expected = "$Пресоединенее: \"$пресоединенее\".";
+            string actual = Master.Parser(testString);
+            TestResult(expected, actual);
+        }
+
+
+    }
+}
